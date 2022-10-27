0f9f09400ff6975a96415e9930b839d1caa2caf1
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-27 12:26:50 -0300

    register user await

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,10 @@
 const User = require("../models/User");
+const { StatusCode } = require("http-status-codes");
+
 const register = async (req, res) => {
-  res.json(req.body);
+  // bad practis
+  const user = await User.create({ ...req.body });
+  res.status(StatusCode.CREATED).json(user);
 };
 
 const login = async (req, res) => {
