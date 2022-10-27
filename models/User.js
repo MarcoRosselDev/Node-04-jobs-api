0f02fbe9a382656716b64ec0e2e967608910342e
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-27 12:51:41 -0300

    breack

--- models/User.js
+++ models/User.js
@@ -3,13 +3,13 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    require: [true, "Please provide name"],
-    minlength: 3,
+    required: [true, "Please provide name"],
     maxlength: 50,
+    minlength: 3,
   },
   email: {
     type: String,
-    require: [true, "Please provide email"],
+    required: [true, "Please provide email"],
     match: [
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
       "Please provide a valid email",
@@ -18,9 +18,8 @@ const UserSchema = new mongoose.Schema({
   },
   password: {
     type: String,
-    require: [true, "Please provide password"],
+    required: [true, "Please provide password"],
     minlength: 6,
-    maxlength: 12,
   },
 });
 
