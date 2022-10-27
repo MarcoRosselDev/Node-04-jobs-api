4640537fea492b9aa99099cd3703ad2e94884835
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-27 12:13:11 -0300

    User name email and password

--- models/User.js
+++ models/User.js
@@ -7,4 +7,21 @@ const UserSchema = new mongoose.Schema({
     minlength: 3,
     maxlength: 50,
   },
+  email: {
+    type: String,
+    require: [true, "Please provide email"],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please provide a valid email",
+    ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: [true, "Please provide password"],
+    minlength: 6,
+    maxlength: 12,
+  },
 });
+
+module.exports = mongoose.model("User", UserSchema);
