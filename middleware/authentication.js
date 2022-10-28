1b1d38f27553071f450a4aba7a7ff6759b178458
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-28 20:32:17 -0300

    authentication.js repit

---
+++ middleware/authentication.js
@@ -0,0 +1,11 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const { UnauthenticatedError } = require("../errors");
+
+const auth = (req, res, next) => {
+  // check header
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new UnauthenticatedError("Authentication invalid");
+  }
+};
