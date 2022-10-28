553184a496964812772807adf3c0de9a8af7d89a
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-28 10:48:35 -0300

    crypt from models

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,7 @@ const { BadRequestError } = require("../errors");
 const bcrypt = require("bcryptjs");
 
 const register = async (req, res) => {
-  /*
+  /* first validation
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
     throw new BadRequestError("please provide name, email and password");
@@ -13,15 +13,20 @@ const register = async (req, res) => {
   */
   // se puede validar aqui pero el error es muy grande y queremos simplificar las cosas solo enviando un mensaje de texto
 
+  /* crypt pass first option
   const { name, email, password } = req.body;
-
+  
   const salt = await bcrypt.genSalt(10); // numero más alto más proceso pero más encryptado
   // 10 es por defecto pero es más que seguro
   const hashedPassword = await bcrypt.hash(password, salt);
-
+  
   const tempUser = { name, email, password: hashedPassword };
-
   const user = await User.create({ ...tempUser });
+  */
+
+  // crypt pass from models User.js
+
+  const user = await User.create({ ...req.body });
   res.status(StatusCodes.CREATED).json({ user });
 };
 
