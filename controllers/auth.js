f133d60df9185ecef8bad554cc12f0f886e9fe88
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-28 11:47:28 -0300

    break

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,33 +4,8 @@ const { BadRequestError } = require("../errors");
 const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
-  /* first validation
-  const { name, email, password } = req.body;
-  if (!name || !email || !password) {
-    throw new BadRequestError("please provide name, email and password");
-  }
-  // bad practis
-  */
-  // se puede validar aqui pero el error es muy grande y queremos simplificar las cosas solo enviando un mensaje de texto
-
-  /* crypt pass first option
-  const { name, email, password } = req.body;
-  
-  const salt = await bcrypt.genSalt(10); // numero más alto más proceso pero más encryptado
-  // 10 es por defecto pero es más que seguro
-  const hashedPassword = await bcrypt.hash(password, salt);
-  
-  const tempUser = { name, email, password: hashedPassword };
-  const user = await User.create({ ...tempUser });
-  */
-
-  // crypt pass from models User.js
-
   const user = await User.create({ ...req.body });
-  const token = jwt.sing({ userId: user._id, name: user.name }, "jwtSecret", {
-    expiresIn: "30d",
-  });
-  res.status(StatusCodes.CREATED).json({ token });
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (req, res) => {
