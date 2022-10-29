494258c9925fc49671b890d9501edff5968c8d7a
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-29 10:54:16 -0300

    error-handler changes for more info and clean

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -10,6 +10,13 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message });
   }
+
+  if (err.code && err.code === 11000) {
+    customError.msg = `duplicate value entered for ${Object.keys(
+      err.keyValue
+    )} field, please choose another value`;
+    customError.statusCode = 400;
+  }
   // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
   return res.status(customError.statusCode).json({ msg: customError.msg });
 };
