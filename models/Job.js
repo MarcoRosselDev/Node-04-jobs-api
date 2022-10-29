ca631ce5302e621099161d66acbc5e0ae1755df0
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-28 21:11:47 -0300

    Job models 2

--- models/Job.js
+++ models/Job.js
@@ -1,17 +1,28 @@
 const mongoose = require("mongoose");
-const JobSchema = new mongoose.Schema({
-  company: {
-    type: String,
-    required: [true, "Please provide company name"],
-    maxlength: 50,
+const JobSchema = new mongoose.Schema(
+  {
+    company: {
+      type: String,
+      required: [true, "Please provide company name"],
+      maxlength: 50,
+    },
+    position: {
+      type: String,
+      required: [true, "Please provide position"],
+      maxlength: 100,
+    },
+    status: {
+      type: String,
+      enum: ["interview", "declined", "pending"],
+      default: "pending",
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "Please provide user"],
+    },
   },
-  position: {
-    type: String,
-    required: [true, "Please provide position"],
-    maxlength: 100,
-  },
-  status: {
-    type: String,
-    enum: ["interview", "declined", "pending"],
-  },
-});
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Job", JobSchema);
