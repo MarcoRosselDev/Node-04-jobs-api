ef686511d189e15056453684874c08c0567f90ec
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-28 21:07:21 -0300

    Job models

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -13,6 +13,7 @@ const auth = (req, res, next) => {
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
     // attach the user to the job routes
+
     req.user = { userId: payload.userId, name: payload.name };
     next();
   } catch (error) {
