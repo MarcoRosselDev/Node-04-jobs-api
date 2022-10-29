ce361e97251eed53329be9a77669fa22ca41be83
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-29 14:12:33 -0300

    rateLimiter

--- app.js
+++ app.js
@@ -22,6 +22,15 @@ const jobsRouter = require("./routes/jobs");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  })
+);
+
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
